9619ce5cd192c590faea5e608a7c3e8d6cec3392
Danil Karpenko <limpix31@gmail.com>
2023-10-01 09:47:40 +0200

    feat: smooth scroll

--- app/entrypoint/app/providers.tsx
+++ app/entrypoint/app/providers.tsx
@@ -5,11 +5,13 @@ import { PropsWithChildren }                   from 'react'
 import { NextUIProvider }                      from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
+import Scroll                                  from './scroll.tsx'
+
 export function Providers({ children }: PropsWithChildren) {
 	return (
 		<NextUIProvider>
 			<NextThemesProvider attribute='class' defaultTheme='dark'>
-				{children}
+				<Scroll>{children}</Scroll>
 			</NextThemesProvider>
 		</NextUIProvider>
 	)
